160b89cb57a0c0789f4cd4c97473a80cd111910b
Masoud <karimi.masoud5494@gmail.com>
2026-07-04 11:25:55 +0330

    pipeline initiated

---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+print('argunemts', sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df["month"] = month
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+print(f'hello pipeline, month= {month}')
+
